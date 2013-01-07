ff30d834a5bef5fb4722cff72735a8a096f494c7
Tyson Matanich <tymat@matanich.com>
2013-01-07 12:59:30 -0800

    Branch from GetViewportWidth
    Updated to also support height and renamed to viewportSize.

---
+++ README.md
@@ -0,0 +1,38 @@
+﻿# viewportSize
+
+Allows you to get the width and height of the CSS viewport using JavaScript.
+
+
+* Author: Tyson Matanich - http://matanich.com
+* License: MIT
+
+
+Demo: http://matanich.com/test/viewport-width/
+
+More Info: http://www.matanich.com/2013/01/07/viewport-size/
+
+Test: http://tysonmatanich.github.com/viewportSize/
+
+
+## Why should I use it?
+
+Most people rely on `window.innerWidth`, `document.documentElement.clientWidth`, or `$(window).width()` which do not always accurately report the viewport width used in CSS media queries. For example, WebKit browsers change the size of their CSS viewport when scroll bars are visible, while most other browsers do not. Since `window.innerWidth` remains constant regardless of the scroll bar state, it is not a good option for use with Chrome or Safari. Additionally, Internet Explorer 6, 7, and 8 do not support `window.innerWidth`. On the other hand, `document.documentElement.clientWidth` changes based on the scroll bar state and therefore is not a good option for Internet Explorer, Firefox, or Opera.
+
+
+## Size and delivery
+
+Currently, `viewportSize.js` compresses to around 609 bytes (~0.59 KB), after minify and gzip. See `viewportSize-min.js` or try one of these online tools: [Microsoft Ajax Minifier]:(http://ajaxmin.codeplex.com/), [Uglify]:(http://marijnhaverbeke.nl/uglifyjs), [Yahoo Compressor]:(http://refresh-sf.com/yui/), or [Closure Compiler](http://closure-compiler.appspot.com/home). Serve with gzip compression.
+
+## Sample
+
+
+```html
+<script type="text/javascript">
+	var width = viewportSize.getWidth();
+	var height = viewportSize.getHeight();
+</script>
+```
+
+## Support
+
+viewportSize supports a broad range of browsers and devices (there are currently no known unsupported browsers).
---
+++ viewportSize.js
@@ -0,0 +1,56 @@
+﻿/*! viewportSize | Author: Tyson Matanich, 2013 | License: MIT */
+(function (window) {
+
+	window.viewportSize = {};
+
+	window.viewportSize.getHeight = function () {
+		return getSize("Height");
+	};
+
+	window.viewportSize.getWidth = function () {
+		return getSize("Width");
+	};
+
+	var getSize = function (Name) {
+		var size;
+		var name = Name.toLowerCase();
+		var document = window.document;
+		var documentElement = document.documentElement;
+		if (window["inner" + Name] === undefined) {
+			// IE6 & IE7 don't have window.innerWidth or innerHeight
+			size = documentElement["client" + Name];
+		}
+		else if (window["inner" + Name] != documentElement["client" + Name]) {
+			// WebKit doesn't include scrollbars while calculating viewport size so we have to get fancy
+
+			// Insert markup to test if a media query will match document.doumentElement["client" + Name]
+			var bodyElement = document.createElement("body");
+			bodyElement.id = "vpw-test-b";
+			bodyElement.style.cssText = "overflow:scroll";
+			var divElement = document.createElement("div");
+			divElement.id = "vpw-test-d";
+			divElement.style.cssText = "position:absolute;top:-1000px";
+			// Getting specific on the CSS selector so it won't get overridden easily
+			divElement.innerHTML = "<style>@media(" + name + ":" + documentElement["client" + Name] + "px){body#vpw-test-b div#vpw-test-d{" + name + ":7px!important}}</style>";
+			bodyElement.appendChild(divElement);
+			documentElement.insertBefore(bodyElement, document.head);
+
+			if (divElement["offset" + Name] == 7) {
+				// Media query matches document.documentElement["client" + Name]
+				size = documentElement["client" + Name];
+			}
+			else {
+				// Media query didn't match, use window["inner" + Name]
+				size = window["inner" + Name];
+			}
+			// Cleanup
+			documentElement.removeChild(bodyElement);
+		}
+		else {
+			// Default to use window["inner" + Name]
+			size = window["inner" + Name];
+		}
+		return size;
+	};
+
+})(this);
